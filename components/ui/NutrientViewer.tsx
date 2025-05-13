3be98ffc79d4c80d8f008cd11658e00c73c76944
JerichoFletcher <jericho.russel17@gmail.com>
2025-05-13 14:16:09 +0700

    feat: presenter view, fullscreen mode

--- components/ui/NutrientViewer.tsx
+++ components/ui/NutrientViewer.tsx
@@ -1,13 +1,34 @@
 "use client";
 
+import type * as NutrientType from "@nutrient-sdk/viewer";
 import * as React from "react";
 
 export function NutrientViewer({
   file, className, ...props
 }: React.ComponentProps<"div"> & { file: File }) {
   const containerRef = React.useRef<HTMLDivElement>(null);
+  const viewerRef = React.useRef<NutrientType.Instance>(null);
   const [fileBuffer, setFileBuffer] = React.useState<ArrayBuffer | null>(null);
 
+  // Hide toolbar and sidebar when on fullscreen mode
+  const onFullScreenChange = () => {
+    viewerRef.current?.setViewState(state => {
+      const isFullscreen = !!document.fullscreenElement;
+      return state
+        .set("showToolbar", !isFullscreen)
+        .set("sidebarMode", isFullscreen ? null : "THUMBNAILS");
+    });
+  };
+
+  // Add event listeners
+  React.useEffect(() => {
+    document.addEventListener("fullscreenchange", onFullScreenChange);
+    return () => {
+      document.removeEventListener("fullscreenchange", onFullScreenChange);
+    };
+  }, []);
+
+  // Request file content read into a raw buffer for direct access
   React.useEffect(() => {
     const reader = new FileReader();
     reader.onload = e => {
@@ -16,28 +37,57 @@ export function NutrientViewer({
     reader.readAsArrayBuffer(file);
   }, [file]);
   
+  // After the file has been read, reload the Nutrient viewer component
   React.useEffect(() => {
     const container = containerRef.current;
     let cleanup = () => {};
     
     (async () => {
-      const Viewer = (await import("@nutrient-sdk/viewer")).default;
-      Viewer.unload(container);
+      const pspdfKit = (await import("@nutrient-sdk/viewer")).default;
+      pspdfKit.unload(container);
 
-      if (container && Viewer && fileBuffer) {
-        Viewer.load({
+      if (container && pspdfKit && fileBuffer) {
+        // Define toolbar items, modify if needed
+        const toolbarItems: NutrientType.ToolbarItem[] = [
+          { type: "search" },
+          { type: "highlighter" },
+        ];
+
+        // Include fullscreen toggle if browser support fullscreen item
+        if (document.fullscreenEnabled) {
+          toolbarItems.push({
+            type: "custom",
+            title: "Toggle fullscreen",
+            onPress: async () => {
+              if (document.fullscreenElement) {
+                document.exitFullscreen();
+              } else {
+                await containerRef.current?.requestFullscreen();
+              }
+            },
+          });
+        }
+
+        pspdfKit.load({
           container,
           document: fileBuffer,
           baseUrl: `${window.location.protocol}//${window.location.host}/`,
-          theme: Viewer.Theme.DARK,
-          initialViewState: new Viewer.ViewState({
-            sidebarMode: Viewer.SidebarMode.THUMBNAILS
+          toolbarItems,
+          toolbarPlacement: "BOTTOM",
+          theme: "DARK",
+          initialViewState: new pspdfKit.ViewState({
+            layoutMode: "SINGLE",
+            scrollMode: "PER_SPREAD",
+            sidebarMode: "THUMBNAILS",
+            sidebarPlacement: "END",
           })
+        }).then(i => {
+          viewerRef.current = i;
         });
       }
 
       cleanup = () => {
-        Viewer.unload(container);
+        pspdfKit.unload(container);
       };
     })();
 
